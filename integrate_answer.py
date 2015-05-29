eeacc33b330466fe540eda0583a8a6fbdea6635a
alicetang0618 <xiaoruit@uchicago.edu>
2015-05-29 09:41:23 -0500

    update

--- integrate_answer.py
+++ integrate_answer.py
@@ -1,6 +1,13 @@
 from mr_parse_answers import MRParseAnswers
-import sys
-import json
+import sys, json, csv
+
+# user list
+USER_FEATURES = ["u_num_answer", "u_qid_answer", "u_pnt_answer_avg",
+                "u_pnt_answer_mid", "u_pnt_answer_min",
+                "u_pnt_answer_max", "u_pnt_answer_std"]
+
+# question list
+QUESTION_FEATURES = ["q_num_answer", "q_first_answerer"]
 
 if __name__ == '__main__':
     user_profile = {}
@@ -12,12 +19,18 @@ if __name__ == '__main__':
         
         for line in runner.stream_output():
             obs, (key, val) = job.parse_output_line(line)
-            if key in ["u_num_answer", "u_avg_pnt_answer", "u_qid_answer"]:
+            if key not in QUESTION_FEATURES:
                 user_profile[obs] = user_profile.get(obs, {})
                 user_profile[obs][key] = val
             else:
                 question_profile[obs] = question_profile.get(obs, {})
                 question_profile[obs][key] = val
 
-    json.dump(user_profile, open("user.txt","w"))
-    json.dump(question_profile, open("question.txt","w"))
+    json.dump(user_profile, open("data/user.txt","w"))
+    json.dump(question_profile, open("data/question.txt","w"))
+    with open('data/user.csv', 'wb') as f:
+        w = csv.writer(f)
+        w.writerows(user_profile.items())
+    with open('data/question.csv', 'wb') as f:
+        w = csv.writer(f)
+        w.writerows(question_profile.items())
