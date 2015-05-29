dc79d629f3ac712667de88b276a21425eedd5726
weiwei334 <lingweic@uchicago.edu>
2015-05-29 13:41:54 -0500

    clustering

--- mr_parse_answers.py
+++ mr_parse_answers.py
@@ -1,6 +1,6 @@
 from mrjob.job import MRJob
-import re
-import sys
+import re, sys
+import numpy as np
 
 # data imported
 Q_ID = 0
@@ -8,63 +8,99 @@ ANSWERERID = 1
 POINTSSNAP = 2
 ANSWERDATE = 3
 
+# missing value
+# use a large negative value if any information is missing
+MISSING = -sys.float_info.max
+
+# user list
+USER_FEATURES = ["u_num_answer", "u_pnt_answer", "u_qid_answer"]
+
+# question list
+QUESTION_FEATURES = ["q_num_answer", "q_first_answerer"]
+
+# helper functions
+def list_helper(l):
+  rv = {}
+  if len(l) == 0:
+    rv["avg"] = MISSING
+    rv["mid"] = MISSING
+    rv["min"] = MISSING
+    rv["max"] = MISSING
+    rv["std"] = MISSING
+  else:
+    rv["avg"] = np.mean(l)
+    rv["mid"] = np.median(l)
+    rv["min"] = np.min(l)
+    rv["max"] = np.max(l)
+    rv["std"] = np.std(l)
+  return rv
+
+
 class MRParseAnswers(MRJob):
   
   def mapper(self, _, line):
     answer = line.strip().replace("|", "\t").split("\t")
-    if len(answer) > 1:
+    if len(answer) > 1 and answer[Q_ID] != "" and answer[ANSWERERID] != "":
       yield answer[ANSWERERID], ("u_num_answer", 1)
-      yield answer[ANSWERERID], ("u_avg_pnt_answer", float(answer[POINTSSNAP]))
+      # missing value
+      point = np.nan
+      if answer[POINTSSNAP] != "":
+        point = float(answer[POINTSSNAP])
+      yield answer[ANSWERERID], ("u_pnt_answer", point)
       yield answer[ANSWERERID], ("u_qid_answer", answer[Q_ID])
       yield answer[Q_ID], ("q_num_answer", 1)
-      yield answer[Q_ID], ("q_first_answerer", (answer[ANSWERERID], float(answer[ANSWERDATE])))
+      date = -MISSING
+      if answer[ANSWERDATE] != "":
+        date = float(answer[ANSWERDATE])
+      yield answer[Q_ID], ("q_first_answerer", (answer[ANSWERERID], date))
 
   def combiner(self, obs, results):
     l = list(results)
-    dic = {}
-    is_user = -1
+    dic = {"u_num_answer": 0, "q_num_answer": 0, "q_first_answerer": (" ", sys.maxint)}
+    is_user = False
     for key, val in l:
+      if key in USER_FEATURES:
+        is_user = True
       # for sums
       if key in ["u_num_answer", "q_num_answer"]:
-        dic[key] = dic.get(key, 0) + val
+        dic[key] += val
       # for min
       elif key in ["q_first_answerer"]:
-        dic[key] = dic.get(key, (" ", sys.maxint))
         if val[1] < dic[key][1]:
           dic[key] = val
-        is_user = False
       # don't use combiner for averages
-      elif key in ["u_avg_pnt_answer", "u_qid_answer"]:
+      elif key in ["u_pnt_answer", "u_qid_answer"]:
         yield obs, (key, val)
-        is_user = True
-    assert(is_user != -1)
     if is_user:
-      yield obs, ("u_num_answer", dic["u_num_answer"])
+        yield obs, ("u_num_answer", dic["u_num_answer"])
     else:
-      yield obs, ("q_num_answer", dic["q_num_answer"])
-      yield obs, ("q_first_answerer", dic["q_first_answerer"])
+        yield obs, ("q_num_answer", dic["q_num_answer"])
+        yield obs, ("q_first_answerer", dic["q_first_answerer"])
 
   def reducer(self, obs, results):
     l = list(results)
-    dic = {}
-    cnt = 0
-    is_user = -1
+    dic = {"u_num_answer": 0, "q_num_answer": 0, \
+          "q_first_answerer": (" ", sys.maxint), "u_qid_answer": []}
+    temp = []
+    is_user = False
     for key, val in l:
-      if key in ["u_num_answer", "u_avg_pnt_answer", "q_num_answer"]:
-        dic[key] = dic.get(key, 0) + val
-        cnt += 1
+      if key in USER_FEATURES:
+        is_user = True
+      if key in ["u_num_answer", "q_num_answer"]:
+        dic[key] += val
       elif key in ["q_first_answerer"]:
-        dic[key] = dic.get(key, (" ", sys.maxint))
         if val[1] < dic[key][1]:
           dic[key] = val
-        is_user = False
       elif key in ["u_qid_answer"]:
-        dic[key] = dic.get(key, []) + [val]
-        is_user = True
-    assert(is_user != -1)
+        dic[key] += [val]
+      elif key in ["u_pnt_answer"]:
+        temp.append(val)
+    temp = filter(lambda x: x==x, temp)
+    point_features = list_helper(temp)
     if is_user:
       yield obs, ("u_num_answer", dic["u_num_answer"])
-      yield obs, ("u_avg_pnt_answer", dic["u_avg_pnt_answer"]/cnt)
+      for key in point_features:
+        yield obs, ("u_pnt_answer_"+key, point_features[key])
       yield obs, ("u_qid_answer", dic["u_qid_answer"])
     else:
       yield obs, ("q_num_answer", dic["q_num_answer"])
