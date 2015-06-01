dcfa0cfacb0f417435eea3b2fdb457f57cf7716b
alicetang0618 <xiaoruit@uchicago.edu>
2015-05-31 20:09:19 -0500

    update

--- integrate_answer.py
+++ integrate_answer.py
@@ -1,29 +1,29 @@
-from parse_answer_new import MRParseAnswers
+from parse_answer import MRParseAnswers
 import sys, json, csv
 
 USER_LIST = 0
 QUESTION_LIST = 1
 
-DIRECTORY = "data/"
+DIRECTORY = "/var/tmp/"
 #"data/"
 #/../../var/tmp/xiaoruit/
 #/var/tmp/
 
 if __name__ == '__main__':
     user_profile = {}
-    question_profile = {}
 
+    print "start"
     f = open(DIRECTORY+"user.csv", "r")
     for line in f:
         l = line.strip().split("|")
         user_profile[l[0]] = l[1:] + [None]*7
     f.close()
 
-    f = open(DIRECTORY+"question.csv", "r")
-    for line in f:
-        l = line.strip().split("|")
-        question_profile[l[0]] = l[1:] + [None]*2
-    f.close()
+    # f = open(DIRECTORY+"question.csv", "r")
+    # for line in f:
+    #     l = line.strip().split("|")
+    #     question_profile[l[0]] = l[1:] + [None]*2
+    # f.close()
 
     print "finish loading user data"
     job = MRParseAnswers(args=sys.argv[1:])
@@ -38,11 +38,11 @@ if __name__ == '__main__':
                     user_profile[obs] = [None]*20 + val
                 else:
                     user_profile[obs][20:] = val
-            else:
-                if obs not in question_profile:
-                    question_profile[obs] = [None]*3 + val
-                else:
-                    question_profile[obs][3:] = val
+            # else:
+            #     if obs not in question_profile:
+            #         question_profile[obs] = [None]*3 + val
+            #     else:
+            #         question_profile[obs][3:] = val
 
     print "start writing user"
     with open(DIRECTORY+'user.csv', 'wb') as myfile:
@@ -50,17 +50,8 @@ if __name__ == '__main__':
         for i in user_profile:
             wr.writerow([i]+user_profile[i])
 
-    print "start writing question"
-    with open(DIRECTORY+'question.csv', 'wb') as myfile:
-        wr = csv.writer(myfile, delimiter="|")
-        for i in question_profile:
-            wr.writerow([i]+question_profile[i])
-
-    # with open(DIRECTORY+"question.csv",'r') as csvinput:
-    #     with open(DIRECTORY+"question2.csv", 'w') as csvoutput:
-    #         writer = csv.writer(csvoutput, delimiter="|")
-    #         for row in csv.reader(csvinput, delimiter="|"):
-    #             if row[0] in question_profile:
-    #                 writer.writerow(row+question_profile[row[0]])
-    #             else:
-    #                 writer.writerow(row+[None]*2)
+    # print "start writing question"
+    # with open(DIRECTORY+'question.csv', 'wb') as myfile:
+    #     wr = csv.writer(myfile, delimiter="|")
+    #     for i in question_profile:
+    #         wr.writerow([i]+question_profile[i])
