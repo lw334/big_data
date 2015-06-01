775b8827f85126f95d8be38ad5680b9ef22a0dde
alicetang0618 <xiaoruit@uchicago.edu>
2015-05-31 21:17:14 -0500

    update

--- integrate_answer.py
+++ integrate_answer.py
@@ -12,12 +12,12 @@ DIRECTORY = "/var/tmp/"
 if __name__ == '__main__':
     user_profile = {}
 
-    print "start"
-    f = open(DIRECTORY+"user.csv", "r")
-    for line in f:
-        l = line.strip().split("|")
-        user_profile[l[0]] = l[1:] + [None]*7
-    f.close()
+    # print "start"
+    # f = open(DIRECTORY+"user.csv", "r")
+    # for line in f:
+    #     l = line.strip().split("|")
+    #     user_profile[l[0]] = l[1:] + [None]*7
+    # f.close()
 
     # f = open(DIRECTORY+"question.csv", "r")
     # for line in f:
@@ -25,30 +25,40 @@ if __name__ == '__main__':
     #     question_profile[l[0]] = l[1:] + [None]*2
     # f.close()
 
-    print "finish loading user data"
+    # print "finish loading user data"
     job = MRParseAnswers(args=sys.argv[1:])
     with job.make_runner() as runner:
+        print "start running"
         runner.run()
         
         print "yielding"
         for line in runner.stream_output():
             obs, (key, val) = job.parse_output_line(line)
-            if key == USER_LIST:
-                if obs not in user_profile:
-                    user_profile[obs] = [None]*20 + val
-                else:
-                    user_profile[obs][20:] = val
+            user_profile[obs] = val
+            # if key == USER_LIST:
+                # if obs not in user_profile:
+                #     user_profile[obs] = [None]*20 + val
+                # else:
+                #     user_profile[obs][20:] = val
             # else:
             #     if obs not in question_profile:
             #         question_profile[obs] = [None]*3 + val
             #     else:
             #         question_profile[obs][3:] = val
 
-    print "start writing user"
-    with open(DIRECTORY+'user.csv', 'wb') as myfile:
-        wr = csv.writer(myfile, delimiter="|")
-        for i in user_profile:
-            wr.writerow([i]+user_profile[i])
+    # print "start writing user"
+    # with open(DIRECTORY+'user.csv', 'wb') as myfile:
+    #     wr = csv.writer(myfile, delimiter="|")
+    #     for i in user_profile:
+    #         wr.writerow(row+[i]+user_profile[i])
+    with open(DIRECTORY+"user.csv",'r') as csvinput:
+        with open(DIRECTORY+"user1.csv", 'w') as csvoutput:
+            writer = csv.writer(csvoutput, delimiter="|")
+            for row in csv.reader(csvinput, delimiter="|"):
+                if row[0] in user_profile:
+                    writer.writerow(row+[user_profile[row[0]]])
+                else:
+                    writer.writerow(row+[None]*7)
 
     # print "start writing question"
     # with open(DIRECTORY+'question.csv', 'wb') as myfile:
